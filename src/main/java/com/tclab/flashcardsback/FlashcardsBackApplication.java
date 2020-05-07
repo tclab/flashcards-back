c6512ca90ef235f7fc32ca2a339cd759994edb72
judtoro <juan.toro@globant.com>
2020-05-07 08:56:31 -0500

    Initial commit

---
+++ src/main/java/com/tclab/flashcardsback/FlashcardsBackApplication.java
@@ -0,0 +1,13 @@
+package com.tclab.flashcardsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tclab/flashcardsback/FlashcardsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tclab.flashcardsback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
